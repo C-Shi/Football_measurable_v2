5ebd1064ca7f4c3e15b43362041b91ee294e6fcc
C-Shi <cshi@ualberta.ca>
2019-07-10 09:05:01 +0800

    create user and comment table

---
+++ db/migrations/20190709130122_create_users_table.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(t){
+    t.increments('id');
+    t.string('email').unique().notNullable();
+    t.string('password').notNullable();
+    t.string('name').notNullable();
+    t.boolean('admin').notNullable().defaultTo(false);
+    t.boolean('coach').notNullable().defaultTo(true);
+    t.boolean('developer').notNullable().defaultTo(false);
+    t.string('profile_url');
+    t.string('password_reset_token');
+    t.timestamp('password_reset_token_expiry');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
---
+++ db/migrations/20190709131545_create_comments_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('comments', function(t) {
+    t.increments('id');
+    t.integer('student_id').unsigned().references('id').inTable('students').onDelete('CASCADE');
+    t.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
+    t.string('content').notNullable();
+    t.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('comments');
+};
