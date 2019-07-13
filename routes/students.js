9d7fae701f2cb4aa8449d4baadaf11cef5490496
C-Shi <cshi@ualberta.ca>
2019-07-14 07:02:17 +0800

    structure basic student REST route

---
+++ routes/students.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/students', (req, res) => {
+
+})
+
+router.get('/students/:id', (req, res) => {
+
+})
+
+router.get('/students/:id/edit', (req, res) => {
+
+})
+
+router.post('/students', (req, res) => {
+
+})
+
+router.put('/students/:id/update', (req, res) => {
+
+})
+
+router.delete('/students/:id', (req, res) => {
+  
+})
+
+module.export = router;
