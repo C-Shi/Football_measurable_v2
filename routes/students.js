85ff12a696176f1266bd0777d01dc888d5e65174
C-Shi <cshi@ualberta.ca>
2019-07-21 12:41:30 +0800

    config cloudinary

--- routes/students.js
+++ routes/students.js
@@ -5,6 +5,30 @@ const requestHelper = require('../lib/requestHelper');
 const commentHelper = require('../lib/commentHelper');
 const performanceHelper = require('../lib/performanceHelper');
 
+// config image upload to cloudinary **************************
+const multer = require('multer');
+const storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+const imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+const upload = multer({ storage: storage, fileFilter: imageFilter})
+
+const cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: 'dhi1ngld5',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+// ******************************************************
+
 // include performance route
 const commentRoute = require('./comments');
 const performanceRoute = require('./performance');
