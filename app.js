6b5d0098c0744985886a801d0047df74b6200610
C-Shi <cshi@ualberta.ca>
2019-07-22 01:20:38 +0800

    login logic

--- app.js
+++ app.js
@@ -7,16 +7,24 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
 
 // app configuration
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 app.use(methodOverride('_method'))
+app.use(cookieSession({
+  name: 'session',
+  secret: 'This ensure the most secure application'
+}))
 
 // requrie routes
 const studentsRoute = require('./routes/students');
+const usersRoute = require('./routes/users');
 app.use('/', studentsRoute);
+app.use('/', usersRoute);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Start at ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router  = express.Router({mergeParams:true})
+const userHelper = require('../lib/userHelper');
+
+router.get('/register', (req, res) => {
+
+})
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+})
+
+router.post('/login', (req, res) => {
+  const email = req.body.email.toLowerCase();
+  const password = req.body.password;
+  userHelper.authenticate(email, password)
+  .then(auth => {
+    if(auth) {
+      // req.session.email = auth.email;
+      // req.session.userId = auth.id;
+      // req.session.name = auth.name;
+      res.send(auth);
+    } else {
+      res.send('The account does not exist or password incorrect')
+    }
+  })
+  .catch(error => {
+    res.send(error.message)
+  })
+  console.log(req.body);
+})
+
+router.post('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/login');
+})
+
+module.exports = router;
