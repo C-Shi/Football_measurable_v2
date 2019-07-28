6719ae3a8df9526635cacc2ac9067a8507076b1a
C-Shi <cshi@ualberta.ca>
2019-07-29 00:02:32 +0800

    reconfig production env

--- knexfile.js
+++ knexfile.js
@@ -22,8 +22,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    client: 'mysql',
+    connection: process.env.CLEARDB_DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
