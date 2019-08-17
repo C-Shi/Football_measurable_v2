0c7ef7611a929ae5c9a976a84db89f42674c93cb
C-Shi <cshi@ualberta.ca>
2019-08-17 12:36:20 +0800

    use javascript to dynamically add year dropdown

--- public/javascript/students/show.js
+++ public/javascript/students/show.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // control delete student form
   $('#delete-student-confirm').on('input', function(e){
     if($(this).attr('data-name') === $(this).val()) {
       $('#delete-student-btn').removeClass('d-none');
@@ -8,6 +9,15 @@ $(document).ready(function(){
       $('#delete-student-btn').addClass('d-none');
       $('#delete-student-comment-error').removeClass('d-none');
       $('#delete-student-comment-success').addClass('d-none');
+    }
+  })
+
+  $('#year-option-dropdown').ready(function(){
+    var currentYear = new Date().getFullYear();
+    var studentId = $('#year-option-dropdown').attr('data-student-id');
+    for(var i = currentYear; i > 2017; i-- ) {
+      var link = $('<a>').addClass('dropdown-item').attr('href', '/students/' + studentId + '?year=' + i).text(i);
+      $('#year-option-dropdown').append(link);
     }
   })
 })
